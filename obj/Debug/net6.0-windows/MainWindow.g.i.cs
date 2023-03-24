e1dc8c668af725671a809a29b73388d489c917dc
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-24 19:59:24 +0700

    1
    1

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace _24_03_2023_WPF_async {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/24-03-2023_WPF_async;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/24-03-2023_WPF_async;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
