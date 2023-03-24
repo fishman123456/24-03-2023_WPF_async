3888039f3159b67b59a589b30afa77c442a46dac
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-24 21:01:14 +0700

    асинхронное считывание, запись

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,20 +26,24 @@ namespace _24_03_2023_WPF_async
         {
             InitializeComponent();
         }
+        // считываем из файла
         private async void button1_Click(object sender, RoutedEventArgs e)
-        { 
+        {
+            string data = null;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "TXT Files(*.txt)|*.txt|All(*.*)|*";
+            dialog.RestoreDirectory = true;
+            dialog.InitialDirectory = dialog.FileName;
+
             try
             {
-                string path = "test.txt";
-                string data = null;
-                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None))
+                if (dialog.ShowDialog() == true)
                 {
-                    using(StreamReader sr = new StreamReader(fs))
-                    {
-                        data = await sr.ReadToEndAsync();
-                    }
+                    string path = dialog.FileName;
+                    using (StreamReader sw = new StreamReader(path) )
+                      
+                    { data = await sw.ReadToEndAsync(); }
                 }
-
                 text_out.Text = data;
             }
             catch (Exception ex)
@@ -47,6 +52,42 @@ namespace _24_03_2023_WPF_async
             }
         }
 
-        
+        // записываем в файл
+        private async void button2_Click(object sender, RoutedEventArgs e)
+        {
+            string data = null;
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "TXT Files(*.txt)|*.txt|All(*.*)|*";
+            dialog.RestoreDirectory = true;
+            dialog.InitialDirectory = dialog.FileName;
+
+            try
+            {
+                if (dialog.ShowDialog() == true)
+                {
+                    string path = dialog.FileName;
+                   
+                    UnicodeEncoding uniencoding = new UnicodeEncoding();
+                    
+                    byte[] result = uniencoding.GetBytes(text_out.Text);
+
+                    using (FileStream SourceStream = File.Open(path, FileMode.OpenOrCreate))
+                    {
+                        SourceStream.Seek(0, SeekOrigin.End);
+                        await SourceStream.WriteAsync(result, 0, result.Length);
+                    }
+
+
+                    
+                }
+                //text_out.Text = data;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
+    
+
